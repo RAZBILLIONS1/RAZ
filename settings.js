56d0885114d09578b17ce29c0c8a7d9de845138d
RAZBILLIONS1 <bakiarazak2004@gmail.com>
2025-04-24 09:24:04 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Razbillions',
   author: 'Bot',
-  botName: "Knight Bot",
+  botName: "Razbillions bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '91702395151X', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '+233599504341', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
